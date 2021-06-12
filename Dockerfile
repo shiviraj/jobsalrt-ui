77c8d3de086bbb5f294a908dd982c6e0e600f968
Shivam Rajput <shivam.rajput@thoughtworks.com>
2021-06-12 18:35:06 +0530

    fix production bugs

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /usr/src/app
 
 COPY . .
 
-RUN npm install --production
+RUN npm install
 RUN npm run build
 
 CMD ["npm", "start"]
--- src/pages/page/disclaimer.js
+++ src/pages/page/disclaimer.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import {Paper, Typography} from '@material-ui/core';
-import useStyles from './useStyles';
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    backgroundColor: theme.palette.common.white, padding: theme.spacing(2),
+    '& p': {margin: theme.spacing(2),},
+    '& ul': {listStyleType: '→',},
+    '& li': {marginLeft: theme.spacing(3),},
+    [theme.breakpoints.down('sm')]: {padding: theme.spacing(1), '& p': {margin: theme.spacing(1),},},
+  },
+}));
 
 const Disclaimer = () => {
   const classes = useStyles();
